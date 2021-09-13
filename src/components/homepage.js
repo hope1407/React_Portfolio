7cb137bbd81cd3c2a51c8c263ef785d7ec045fad
Hope <johnhope354@gmail.com>
2021-09-12 20:52:56 -0500

    linked bootstrap with my react app

--- src/components/homepage.js
+++ src/components/homepage.js
@@ -1,10 +1,38 @@
-import React from 'react';
-import Button from 'react-bootstrap/Button';
+import React from "react";
+import Nav from "react-bootstrap/Nav";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 function Homepage() {
-    return (
-        <div><Button>Show Toast</Button></div>
-    )
+  return (
+    <div>
+      <Nav className="justify-content-end" activeKey="/home">
+        <Nav.Item>
+          <Nav.Link href="/home">Home</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="link-1">About Me</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="link-2">Portfolio</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="link-3">Contact</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="link-4">Resume</Nav.Link>
+        </Nav.Item>
+      </Nav>
+      <Container>
+        <Row>
+          <Col>1 of 3</Col>
+          <Col>2 of 3</Col>
+          <Col>3 of 3</Col>
+        </Row>
+      </Container>
+    </div>
+  );
 }
 
-export default Homepage
+export default Homepage;
