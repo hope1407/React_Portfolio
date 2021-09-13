2c81fcc50408dc2eb08a2ff579c847deb37cb69e
Hope <johnhope354@gmail.com>
2021-09-12 20:16:38 -0500

    pushing initial react app

---
+++ src/components/homepage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+
+function Homepage() {
+    return (
+        <div><Button>Show Toast</Button></div>
+    )
+}
+
+export default Homepage
