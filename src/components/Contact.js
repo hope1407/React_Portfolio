dc2256afc25e427b2f0985dccddef3a993ef478f
Hope <johnhope354@gmail.com>
2021-09-20 21:09:30 -0500

    working on logic for element rendering

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,20 +1,52 @@
 import React, { useRef, useState } from "react";
 import emailjs from "emailjs-com";
 
-import { validateEmail } from '../utils/helpers';
+import { validateEmail } from "../utils/helpers";
 
 export default function ContactUs() {
+  const [email, setEmail] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const [name, setName] = useState("");
+  const [message, setMessage] = useState("");
+  const [requiredField, setRequiredField] = useState("");
 
-    const [email] = useState('');
-    const [errorMessage, setErrorMessage] = useState('');
+  const handleBlur = (e) => {
+    const { target } = e;
+    const inputType = target.name;
+    const inputValue = target.value;
+
+    if (inputType === "user_name") {
+      setName(inputValue);
+    } else if (inputType === "message") {
+      setMessage(inputValue);
+    }
+
+    if (!name || !message) {
+      setRequiredField("Please fill out all fields!");
+    } else {
+      setRequiredField("");
+    }
+  };
+
+  const handleInputChange = (e) => {
+    const { target } = e;
+    const inputType = target.name;
+    const inputValue = target.value;
+
+    if (inputType === "user_email") {
+      setEmail(inputValue);
+    }
+
+    if (!validateEmail(inputValue)) {
+      setErrorMessage("Please enter a valid email address.");
+    } else {
+      setErrorMessage("");
+    }
+  };
 
   const form = useRef();
 
   const sendEmail = () => {
-    
-      if (!validateEmail(email)) {
-          setErrorMessage('Please Enter a')
-      }
     emailjs
       .sendForm(
         "service_bjvev9x",
@@ -36,14 +68,30 @@ export default function ContactUs() {
   };
 
   return (
-    <form className="emailform" ref={form} onSubmit={sendEmail}>
-      <label>Name</label>
-      <input type="text" name="user_name" id="nameemailfield" />
-      <label>Email</label>
-      <input type="email" name="user_email" id="nameemailfield" />
-      <label>Message</label>
-      <textarea name="message" id="messagefield" />
-      <input type="submit" value="Send" id="sendbtn" onClick={thankyou} />
-    </form>
+    <div>
+      <form className="emailform" ref={form} onSubmit={sendEmail}>
+        <p>{requiredField}</p>
+        <label>Name</label>
+        <input
+          onBlur={handleBlur}
+          type="text"
+          name="user_name"
+          id="nameemailfield"
+        />
+        <label>Email</label>
+        <input
+          value={email}
+          onBlur={handleBlur}
+          onChange={handleInputChange}
+          type="email"
+          name="user_email"
+          id="nameemailfield"
+        />
+        <p>{errorMessage}</p>
+        <label>Message</label>
+        <textarea onBlur={handleBlur} name="message" id="messagefield" />
+        <input type="submit" value="Send" id="sendbtn" onClick={thankyou} />
+      </form>
+    </div>
   );
 }
